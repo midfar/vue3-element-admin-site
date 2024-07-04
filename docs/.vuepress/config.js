b83a0ec7ab9886c1d2be3ebe3a266a4e28c47280
midfar <midfar@qq.com>
2024-07-04 18:52:44 +0800

    feat: update meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -197,11 +197,13 @@ module.exports = {
   locales: {
     '/': {
       lang: 'en-US',
-      description: 'A magical vue3 admin'
+      description:
+        'vue3-element-admin is a production-ready front-end solution for admin interfaces. It based on vue3 and use the UI Toolkit element-plus.'
     },
     '/zh/': {
       lang: 'zh-CN',
-      description: 'A magical vue3 admin'
+      description:
+        'vue3-element-admin 是一个后台前端解决方案，它基于 vue3 和 element-plus 实现。它使用了最新的前端技术栈，内置了 i18 国际化解决方案，动态路由，权限验证，提炼了典型的业务模型，提供了丰富的功能组件，它可以帮助你快速搭建企业级中后台产品原型。相信不管你的需求是什么，本项目都能帮助到你。'
     }
   },
   configureWebpack: {
